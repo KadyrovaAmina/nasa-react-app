278c41f2e7c6ac90e29c36cfe806762bf952e8de
Kadyrova Amina <99501882+KadyrovaAmina@users.noreply.github.com>
2024-07-17 21:22:06 +0600

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,7 @@ export const Main = (props) => {
     const {data} = props
   return (
     <div className="imgContainer">
-      <img src={data?.hdurl} alt={data?.title || 'bg-img'} className="bgImage" />;
+      <img src={data?.url} alt={data?.title || 'bg-img'} className="bgImage" />;
     </div>
   );
 };
